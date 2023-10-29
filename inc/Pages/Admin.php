a27ec99ce81933b0bf9114f219141f58bdbd0f6b
Murtaja Ahmed Noyeem <42427285+Muurtaja@users.noreply.github.com>
2023-10-29 17:32:01 +0600

    Modular Administration Page

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -5,21 +5,58 @@
 
 namespace Inc\Pages;
 
+use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
 
 class Admin extends BaseController
 {
 
+    public $settings;
+    public $pages;
+
+    public function __construct()
+    {
+        $this->settings = new SettingsApi();
+        $this->pages = [
+            [
+                'page_title' => 'Muu Plugin',
+                'menu_title' => 'Muu',
+                'capability' => 'manage_options',
+                'menu_slug'  => 'muu_plugin',
+                'callback'   => function () {
+                    echo "<h1>Muu plugin</h1>";
+                },
+                'icon_url'   => 'dashicons-store',
+                'position'   => 110
+            ],
+            [
+                'page_title' => 'Test Plugin',
+                'menu_title' => 'Test',
+                'capability' => 'manage_options',
+                'menu_slug'  => 'test_plugin',
+                'callback'   => function () {
+                    echo "<h1>Test plugin</h1>";
+                },
+                'icon_url'   => 'dashicons-external',
+                'position'   => 110
+            ]
+        ];
+    }
+
     public function register()
     {
-        add_action('admin_menu', [$this, 'add_admin_pages']);
+
+        $this->settings->addPages($this->pages)->register();
     }
-    public function add_admin_pages()
-    {
-        add_menu_page('Muu Plugin', 'Muu', 'manage_options', 'muu_plugin', [$this, 'admin_index'], 'dashicons-store', 110);
-    }
-    function admin_index()
-    {
-        require_once "{$this->plugin_path}templates/admin.php";
-    }
+
+
+    // public function add_admin_pages()
+    // {
+    //     add_menu_page('Muu Plugin', 'Muu', 'manage_options', 'muu_plugin', [$this, 'admin_index'], 'dashicons-store', 110);
+    // }
+
+    // function admin_index()
+    // {
+    //     require_once "{$this->plugin_path}templates/admin.php";
+    // }
 }
---
+++ readme.md
@@ -0,0 +1,14 @@
+# MuuPlugin 0.0.1
+
+Full list of sections and features we will build:
+
+* Modular Administration Area
+* CPT Manager
+* Custom Taxonomy Manager
+* Widget to Upload and Display Media in sidebars
+* Post and Pages Gallery Integration
+* Testimonial section: Comment in the front-end, Admins can approve comments, select which comments to display
+* Custom template sections
+* Ajax based Login/Register system
+* Membership protected area
+* Chat system
